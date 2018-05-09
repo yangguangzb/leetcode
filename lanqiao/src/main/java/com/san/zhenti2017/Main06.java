bc32241532c229aaa9034456af2d70379f7c0f6a
yangguangzb <2570886020@qq.com>
2018-05-09 15:37:57 +0800

    36进制

---
+++ lanqiao/src/main/java/com/san/zhenti2017/Main06.java
@@ -0,0 +1,13 @@
+package com.san.zhenti2017;
+/**
+ * 36进制：是乘以36不是35
+ * @description
+ * @author zhangbiao
+ * @time 2018-5-9 下午3:37:13
+ */
+public class Main06 {
+	//答案：1040254
+	public static void main(String[] args) {
+		System.out.println(34+23*36+10*36*36+22*36*36*36);
+	}
+}
