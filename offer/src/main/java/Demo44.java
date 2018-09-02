6c0cfd88701ed31bd0f2aa13c1d13ac2b9f7c97d
yangguangzb <2570886020@qq.com>
2018-09-02 15:23:03 +0800

    【知识迁移能力】翻转单词顺序列

---
+++ offer/src/main/java/Demo44.java
@@ -0,0 +1,30 @@
+/**
+ * @Author zb
+ * @Date 2018/9/2
+ * 【知识迁移能力】翻转单词顺序列
+ *
+ * 牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。
+ * 同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。
+ * 例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，
+ * 正确的句子应该是“I am a student.”。Cat对一一的翻转这些单词顺序可不在行，
+ * 你能帮助他么？
+ */
+public class Demo44 {
+
+    public static void main(String[] args) {
+        System.out.println(ReverseSentence(""));
+    }
+
+    public static String ReverseSentence(String str) {
+        if(str.trim().equals("") || str.length() == 0){
+            return str;
+        }
+        String[] strs = str.split(" ");
+        StringBuffer sb = new StringBuffer("");
+        for(int i = strs.length - 1; i >= 0; i--){
+            sb.append(strs[i]+" ");
+        }
+        return sb.toString();
+    }
+
+}
